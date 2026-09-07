1a1fbfbd4ec79f96d5b32a9ea54f401ea778e68e
Aaditya Sahani <sahaniaadi98670@gmail.com>
2026-09-07 11:52:28 +0530

    Fix cookie settings for cross-site auth

--- src/utils/token.util.js
+++ src/utils/token.util.js
@@ -1,20 +1,16 @@
-const jwt = require('jsonwebtoken');
-
-
-const generateTokenSetCookie =  (payload, res) => {
+const generateTokenSetCookie = (payload, res) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+
+  const isProduction = process.env.NODE_ENV === 'production';
 
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Set to true in production
-    sameSite: 'strict', // Adjust based on your requirements
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    secure: isProduction,
+    sameSite: isProduction ? 'none' : 'lax',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   return token;
-
 }
 
-module.exports = {
-  generateTokenSetCookie
-};
+module.exports = { generateTokenSetCookie };
